08f7650f92e594fa39f96139d313dd8f148279f8
Steve Bargelt <steve@bargelt.com>
2021-05-20 09:09:46 -0700

    EventGrid trigger

--- src/MeatGeek.Iot.WorkerApi/WorkerApiFunctions.cs
+++ src/MeatGeek.Iot.WorkerApi/WorkerApiFunctions.cs
@@ -4,43 +4,40 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Extensions.Logging;
 
-using MeatGeek.Shared;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+
+// using MeatGeek.Shared;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Host;
 
 namespace MeatGeek.IoT.WorkerApi
 {
     public static class WorkerApiFunctions
     {
-        private static readonly IEventGridSubscriberService EventGridSubscriberService = new EventGridSubscriberService();
+        //private static readonly IEventGridSubscriberService EventGridSubscriberService = new EventGridSubscriberService();
         //private static readonly ICategoriesService CategoriesService = new CategoriesService(new CategoriesRepository(), new ImageSearchService(new Random(), new HttpClient()), new SynonymService(new HttpClient()), new EventGridPublisherService());
 
-        [FunctionName("AddSession")]
-        public static async Task<IActionResult> AddSession(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]HttpRequest req,
+        [FunctionName("SessionCreated")]
+        public static async Task<IActionResult> SessionCreated(
+            [EventGridTrigger]EventGridEvent eventGridEvent,
             ILogger log)
         {
-            log.LogInformation("AddSession Called");
-
-            // authenticate to Event Grid if this is a validation event
-            var eventGridValidationOutput = EventGridSubscriberService.HandleSubscriptionValidationEvent(req);
-            if (eventGridValidationOutput != null)
-            {
-                log.LogInformation("Responding to Event Grid subscription verification.");
-                return eventGridValidationOutput;
-            }
+        
+            log.LogInformation("SessionCreated Called");
+            log.LogInformation(eventGridEvent.Data.ToString());
             
             try
             {
-                var (eventGridEvent, smokerId, sessionId) = EventGridSubscriberService.DeconstructEventGridMessage(req);
+                //var (eventGridEvent, smokerId, sessionId) = EventGridSubscriberService.DeconstructEventGridMessage(req);
                 
                 // process the category item
                 //await CategoriesService.ProcessAddItemEventAsync(eventGridEvent, uId);
                 log.LogInformation("this worked, I think!!!!!!!");
-                log.LogInformation("SessionID = " + sessionId);
+                //log.LogInformation("SessionID = " + sessionId);
 
                 return new OkResult();
             }
