202f1ab59223592015b1da5c9c478c94055c2b2d
Steve Bargelt <steve@bargelt.com>
2021-05-21 06:50:29 -0700

    Renaming function

--- src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
+++ src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
@@ -2,34 +2,32 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Microsoft.Extensions.Logging;
-
-using Microsoft.Azure.EventGrid.Models;
-using Microsoft.Azure.WebJobs;
-
-// using MeatGeek.Shared;
-
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+// using MeatGeek.Shared;
+
 
 namespace MeatGeek.IoT.WorkerApi
 {
-    public static class WorkerApiFunctions
+    public static class SessionCreated
     {
         //private static readonly IEventGridSubscriberService EventGridSubscriberService = new EventGridSubscriberService();
         //private static readonly ICategoriesService CategoriesService = new CategoriesService(new CategoriesRepository(), new ImageSearchService(new Random(), new HttpClient()), new SynonymService(new HttpClient()), new EventGridPublisherService());
 
         [FunctionName("SessionCreated")]
-        public static async Task<IActionResult> SessionCreated(
+        public static async Task<IActionResult> Run(
             [EventGridTrigger]EventGridEvent eventGridEvent,
             ILogger log)
         {
         
             log.LogInformation("SessionCreated Called");
-            log.LogInformation(eventGridEvent.Data.ToString());
+            //log.LogInformation(eventGridEvent.Data.ToString());
             
             try
             {
