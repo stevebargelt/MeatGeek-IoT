94fb5860e2172500b55720893ab2360e5d50501a
Steve Bargelt <steve@bargelt.com>
2021-05-22 09:40:47 -0700

    Refactoring. Removed xxxDI variants and made the DI versions the default

--- src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
+++ src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
@@ -1,42 +1,42 @@
 using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-
-// using MeatGeek.Shared;
-
+using MeatGeek.Shared;
+using MeatGeek.Shared.EventSchemas.Sessions;
 
 namespace MeatGeek.IoT.WorkerApi
 {
     public static class SessionCreated
     {
-        //private static readonly IEventGridSubscriberService EventGridSubscriberService = new EventGridSubscriberService();
-        //private static readonly ICategoriesService CategoriesService = new CategoriesService(new CategoriesRepository(), new ImageSearchService(new Random(), new HttpClient()), new SynonymService(new HttpClient()), new EventGridPublisherService());
+        private const string METHOD_NAME = "SessionCreated";
+        private const string MODULE_NAME = "";
 
         [FunctionName("SessionCreated")]
         public static void Run(
             [EventGridTrigger]EventGridEvent eventGridEvent,
             ILogger log)
         {
-        
             log.LogInformation("SessionCreated Called");
             log.LogInformation(eventGridEvent.Data.ToString());
             
             try
             {
-                //var (eventGridEvent, smokerId, sessionId) = EventGridSubscriberService.DeconstructEventGridMessage(req);
-                
-                // process the category item
-                //await CategoriesService.ProcessAddItemEventAsync(eventGridEvent, uId);
-                log.LogInformation("this worked, I think!!!!!!!");
-                //log.LogInformation("SessionID = " + sessionId);
+                SessionCreatedEventData sessionCreatedEventData;
+
+                if (eventGridEvent.Data is SessionCreatedEventData)
+                {
+                    sessionCreatedEventData = (SessionCreatedEventData)eventGridEvent.Data;
+                }
+                else
+                {
+                    log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data is not in expected format.");
+                    throw new InvalidOperationException("Event Grid Data is not in expected format.");
+                }
+                var smokerId = sessionCreatedEventData.SmokerId;
+                var sessionId = sessionCreatedEventData.Id;
+                log.LogInformation("SmokerID = " + smokerId);
+                log.LogInformation("SessionID = " + sessionId);
 
             }
             catch (Exception ex)
