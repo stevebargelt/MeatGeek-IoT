9606d4e2f1e83cd1c68130b7c5efafaa2611e973
Steve Bargelt <steve@bargelt.com>
2021-05-22 17:29:14 -0700

    DI. Added actual IoT C2D message to SessionCreated

--- src/MeatGeek.IoT.Functions/SetTelemetryInterval.cs
+++ src/MeatGeek.IoT.Functions/SetTelemetryInterval.cs
@@ -5,41 +5,38 @@ using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Devices;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 namespace MeatGeek.IoT
 {
-
-    public static class SetTelemetryInterval
+    public class SetTelemetryInterval
     {
-        private static IConfigurationRoot config = new ConfigurationBuilder()
-                .SetBasePath(Environment.CurrentDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
+        private readonly ILogger<SetTelemetryInterval> _log;
+        private TelemetryClient _telemetry;
+
+        public SetTelemetryInterval(ILogger<SetTelemetryInterval> log, TelemetryClient telemetry) {
+            _log = log;
+            _telemetry = telemetry;
+        }
         
-        // AppInsights TelemetryClient
-        private static TelemetryClient telemetry = new TelemetryClient();                
         private static ServiceClient _iothubServiceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("MeatGeekIoTServiceConnection", EnvironmentVariableTarget.Process));
         private const string METHOD_NAME = "TelemetryInterval";
         private const string MODULE_NAME = "Telemetry";
 
         [FunctionName("telemetryinterval")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "telemetryinterval/{device}")][FromBody] string value, 
-            string device,
-            ILogger log)
+            string device)
         {
-            log.LogInformation($"telemetryinterval function executed at: {DateTime.Now}");
+            _log.LogInformation($"telemetryinterval function executed at: {DateTime.Now}");
 
             if (string.IsNullOrEmpty(value)) // TODO: Check value for vaild range... 180 - 400 or whatever.
             {
-                log.LogInformation("Request Body value IsNullOrEmpty. Returning BadRequest = Missing body value. Body should be a single integer.");
+                _log.LogInformation("Request Body value IsNullOrEmpty. Returning BadRequest = Missing body value. Body should be a single integer.");
                 return new BadRequestObjectResult("Missing body value. Body should be a single integer.");
             }
-            log.LogInformation("Request Body value = " + value);
+            _log.LogInformation("Request Body value = " + value);
             var methodRequest = new CloudToDeviceMethod(METHOD_NAME, TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(15));
 
             // Generate a Guid as the correlationId which we use to track the message through the pipeline
@@ -53,33 +50,33 @@ namespace MeatGeek.IoT
                 { "processingStep", "1-telemetryinterval"}
             };
             
-            telemetry.TrackEvent("10-StartMethodInvocation", telemetryProperties);
+            _telemetry.TrackEvent("10-StartMethodInvocation", telemetryProperties);
 
             try
             {
-                log.LogInformation($"Invoking method telemetryinterval on module {device}/{MODULE_NAME}. CorrelationId={correlationId}");
+                _log.LogInformation($"Invoking method telemetryinterval on module {device}/{MODULE_NAME}. CorrelationId={correlationId}");
                 // Invoke direct method
                 var result = await _iothubServiceClient.InvokeDeviceMethodAsync(device, MODULE_NAME, methodRequest).ConfigureAwait(false);
 
                 telemetryProperties.Add("MethodReturnCode", $"{result.Status}");
                 if (IsSuccessStatusCode(result.Status))
                 {
-                    telemetry.TrackEvent("11-SuccessfulMethodInvocation", telemetryProperties);
-                    log.LogInformation($"[{device}/{MODULE_NAME}] Successful direct method call result code={result.Status}");
+                    _telemetry.TrackEvent("11-SuccessfulMethodInvocation", telemetryProperties);
+                    _log.LogInformation($"[{device}/{MODULE_NAME}] Successful direct method call result code={result.Status}");
                     
                 }
                 else
                 {
-                    telemetry.TrackEvent("15-UnsuccessfulMethodInvocation", telemetryProperties);
-                    log.LogWarning($"[{device}/{MODULE_NAME}] Unsuccessful direct method call result code={result.Status}");
+                    _telemetry.TrackEvent("15-UnsuccessfulMethodInvocation", telemetryProperties);
+                    _log.LogWarning($"[{device}/{MODULE_NAME}] Unsuccessful direct method call result code={result.Status}");
                 }
                 return new ObjectResult(result);
             }
             catch (Exception e)
             {
                 telemetryProperties.Add("methodInvocationException", e.Message);
-                telemetry.TrackEvent("16-ExceptionInMethodInvocation", telemetryProperties);
-                log.LogError(e, $"[{device}/{MODULE_NAME}] Exeception on direct method call");
+                _telemetry.TrackEvent("16-ExceptionInMethodInvocation", telemetryProperties);
+                _log.LogError(e, $"[{device}/{MODULE_NAME}] Exeception on direct method call");
                 return new BadRequestObjectResult("Exception was caught in function app.");
             }
         }
--- src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
+++ src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
@@ -1,24 +1,35 @@
 using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using MeatGeek.Shared;
 using MeatGeek.Shared.EventSchemas.Sessions;
+//using MeatGeek.IoT.WorkerApi.Configurations;
 
 namespace MeatGeek.IoT.WorkerApi
 {
-    public static class SessionCreated
+    public class SessionCreated
     {
+
+        private readonly ILogger<SessionCreated> _log;
+
+        public SessionCreated(ILogger<SessionCreated> log) {
+            _log = log;
+        }
+
+        private static ServiceClient _iothubServiceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("MeatGeekIoTServiceConnection", EnvironmentVariableTarget.Process));
         private const string METHOD_NAME = "SessionCreated";
-        private const string MODULE_NAME = "";
+        private const string MODULE_NAME = "Telemetry";
 
         [FunctionName("SessionCreated")]
-        public static void Run(
-            [EventGridTrigger]EventGridEvent eventGridEvent,
-            ILogger log)
+        public async Task<IActionResult> Run(
+            [EventGridTrigger]EventGridEvent eventGridEvent)
         {
-            log.LogInformation("SessionCreated Called");
-            log.LogInformation(eventGridEvent.Data.ToString());
+            _log.LogInformation("SessionCreated Called");
+            _log.LogInformation(eventGridEvent.Data.ToString());
             
             try
             {
@@ -27,25 +38,54 @@ namespace MeatGeek.IoT.WorkerApi
                 if (eventGridEvent.Data is SessionCreatedEventData)
                 {
                     sessionCreatedEventData = (SessionCreatedEventData)eventGridEvent.Data;
+                    _log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data --> SessionCreatedEventData");
                 }
                 else
                 {
-                    log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data is not in expected format.");
+                    _log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data is not in expected format.");
                     throw new InvalidOperationException("Event Grid Data is not in expected format.");
                 }
                 var smokerId = sessionCreatedEventData.SmokerId;
                 var sessionId = sessionCreatedEventData.Id;
-                log.LogInformation("SmokerID = " + smokerId);
-                log.LogInformation("SessionID = " + sessionId);
+                _log.LogInformation("SmokerID = " + smokerId);
+                _log.LogInformation("SessionID = " + sessionId);
+                var methodRequest = new CloudToDeviceMethod(METHOD_NAME, TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(15));
+                methodRequest.SetPayloadJson(sessionId);
+
+                try
+                {
+                    _log.LogInformation($"Invoking method telemetryinterval on module {smokerId}/{MODULE_NAME}.");
+                    // Invoke direct method
+                    var result = await _iothubServiceClient.InvokeDeviceMethodAsync(smokerId, MODULE_NAME, methodRequest).ConfigureAwait(false);
+
+                    if (IsSuccessStatusCode(result.Status))
+                    {
+                        _log.LogInformation($"[{smokerId}/{MODULE_NAME}] Successful direct method call result code={result.Status}");
+                        
+                    }
+                    else
+                    {
+                        _log.LogWarning($"[{smokerId}/{MODULE_NAME}] Unsuccessful direct method call result code={result.Status}");
+                    }
+                    return new ObjectResult(result);
+                }
+                catch (Exception e)
+                {
+                    _log.LogError(e, $"[{smokerId}/{MODULE_NAME}] Exeception on direct method call");
+                    return new BadRequestObjectResult("Exception was caught in function app.");
+                }               
 
             }
             catch (Exception ex)
             {
-                log.LogError(ex, "<-- SessionCreated Event Grid Trigger: Unhandled exception");
-                
+                _log.LogError(ex, "<-- SessionCreated Event Grid Trigger: Unhandled exception");
+                return new BadRequestObjectResult("SessionCreated: Unhandled Exception in function app.");
             }
         }
-
         
+        private static bool IsSuccessStatusCode(int statusCode)
+        {
+            return (statusCode >= 200) && (statusCode <= 299);
+        }        
     }
 }
