9c8e72d59bed6817e55fea78a0a972921e90c844
Steve Bargelt <steve@bargelt.com>
2021-05-24 15:54:27 -0700

    EventGridTrigger requires a logger to be passed in ???

--- src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
+++ src/MeatGeek.Iot.WorkerApi/SessionCreated.cs
@@ -14,11 +14,6 @@ namespace MeatGeek.IoT.WorkerApi
     public class SessionCreatedTrigger
     {
 
-        private static ILogger<SessionCreatedTrigger> _log;
-
-        public SessionCreatedTrigger(ILogger<SessionCreatedTrigger> log) {
-            _log = log;
-        }
 
         private static ServiceClient _iothubServiceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("MeatGeekIoTServiceConnection", EnvironmentVariableTarget.Process));
         private const string METHOD_NAME = "SessionCreated";
@@ -26,10 +21,11 @@ namespace MeatGeek.IoT.WorkerApi
 
         [FunctionName("SessionCreated")]
         public static async Task Run(
-            [EventGridTrigger]EventGridEvent eventGridEvent)
+            [EventGridTrigger]EventGridEvent eventGridEvent,
+            ILogger log)
         {
-            _log.LogInformation("SessionCreated Called");
-            _log.LogInformation(eventGridEvent.Data.ToString());
+            log.LogInformation("SessionCreated Called");
+            log.LogInformation(eventGridEvent.Data.ToString());
             
             try
             {
@@ -38,47 +34,47 @@ namespace MeatGeek.IoT.WorkerApi
                 if (eventGridEvent.Data is SessionCreatedEventData)
                 {
                     sessionCreatedEventData = (SessionCreatedEventData)eventGridEvent.Data;
-                    _log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data --> SessionCreatedEventData");
+                    log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data --> SessionCreatedEventData");
                 }
                 else
                 {
-                    _log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data is not in expected format.");
+                    log.LogInformation("SessionCreated Event Grid Trigger: Event Grid Data is not in expected format.");
                     throw new InvalidOperationException("Event Grid Data is not in expected format.");
                 }
                 var smokerId = sessionCreatedEventData.SmokerId;
                 var sessionId = sessionCreatedEventData.Id;
-                _log.LogInformation("SmokerID = " + smokerId);
-                _log.LogInformation("SessionID = " + sessionId);
+                log.LogInformation("SmokerID = " + smokerId);
+                log.LogInformation("SessionID = " + sessionId);
                 var methodRequest = new CloudToDeviceMethod(METHOD_NAME, TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(15));
                 methodRequest.SetPayloadJson(sessionId);
 
                 try
                 {
-                    _log.LogInformation($"Invoking method telemetryinterval on module {smokerId}/{MODULE_NAME}.");
+                    log.LogInformation($"Invoking method telemetryinterval on module {smokerId}/{MODULE_NAME}.");
                     // Invoke direct method
                     var result = await _iothubServiceClient.InvokeDeviceMethodAsync(smokerId, MODULE_NAME, methodRequest).ConfigureAwait(false);
 
                     if (IsSuccessStatusCode(result.Status))
                     {
-                        _log.LogInformation($"[{smokerId}/{MODULE_NAME}] Successful direct method call result code={result.Status}");
+                        log.LogInformation($"[{smokerId}/{MODULE_NAME}] Successful direct method call result code={result.Status}");
                         
                     }
                     else
                     {
-                        _log.LogWarning($"[{smokerId}/{MODULE_NAME}] Unsuccessful direct method call result code={result.Status}");
+                        log.LogWarning($"[{smokerId}/{MODULE_NAME}] Unsuccessful direct method call result code={result.Status}");
                     }
                     //return new ObjectResult(result);
                 }
                 catch (Exception e)
                 {
-                    _log.LogError(e, $"[{smokerId}/{MODULE_NAME}] Exeception on direct method call");
+                    log.LogError(e, $"[{smokerId}/{MODULE_NAME}] Exeception on direct method call");
                     //return new BadRequestObjectResult("Exception was caught in function app.");
                 }               
 
             }
             catch (Exception ex)
             {
-                _log.LogError(ex, "<-- SessionCreated Event Grid Trigger: Unhandled exception");
+                log.LogError(ex, "<-- SessionCreated Event Grid Trigger: Unhandled exception");
                 //return new BadRequestObjectResult("SessionCreated: Unhandled Exception in function app.");
             }
         }
